274ed30fb1680adad418c3c63d24a1c4b55fa65f
blehye <hhw0850@naver.com>
2022-09-10 17:57:32 +0900

    update arrow rotate

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 window.onload = function () {
   const question = document.querySelectorAll(".main-content-detail-question");
   const answer = document.querySelectorAll(".main-content-detail-answer");
+  const arrow = document.querySelectorAll(".main-content-detail-question img");
   for (let i = 0; i < question.length; i++) {
     question[i].addEventListener("click", function () {
-      console.log(i + "클릭됨");
-
       question[i].classList.toggle("question-bold");
       answer[i].classList.toggle("display-block");
+      arrow[i].classList.toggle("rotate");
     });
   }
 };
